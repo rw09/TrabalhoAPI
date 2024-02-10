0858be2dada469751c60e08c2b9ad00bdb442b1f
rw09 <rodrigosouza170487@gmail.com>
2024-02-10 16:47:43 -0300

    Adicionado get para pegar as informações da liga do time

--- SRC/controllers/controllerPaises.js
+++ SRC/controllers/controllerPaises.js
@@ -16,6 +16,41 @@ server.get('/paises/:pais', (req, res) => {
     return res.json(dadosPais)
 })
 
+//Pegar a liga do time
+server.get('/paises/:pais/:liga', (req, res) => {
+
+    const nomePais = req.params.pais
+
+    const nomeLiga = req.params.liga
+
+    const dadosPais = dadosPaises.Pais.find(p => p.nome === nomePais)
+
+    const dadosLiga = dadosPais.campeonatos.find(c => c.nome === nomeLiga)
+
+    return res.json(dadosLiga)
+})
+
+
+//Pegar todas as competições de um país
+// server.get('/paises/:pais/', (req, res) => {
+
+//     const nomePais = req.params.pais
+
+//     const nomeCompeticao = req.params.competicao
+
+//     const dadosPais = dadosPaises.Pais.find(p => p.nome === nomePais)
+
+//     const dadosCompeticao = dadosPais.campeonatos.find(c => c.nome === nomeCompeticao)
+
+//     const competicoesEncontradas = dadosPais.campeonatos.concat(dadosPais.copas).filter(c => c.nome === nomeCompeticao);
+
+//     if(!dadosCompeticao) {
+//         dadosCompeticao = dadosPais.copas.find(c => c.nome === nomeCompeticao)
+//     }
+
+//     return res.json(competicoesEncontradas)
+// })
+
 
 server.get('/times/:pais/:id', (req, res) => {
     
