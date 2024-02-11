082c52b5888d09f5eba4f4dd862635110d58b475
rw09 <rodrigosouza170487@gmail.com>
2024-02-10 21:40:55 -0300

    Seções: cabeçalho, resumo, uniformes e estádio adicionados no dialog

--- SRC/clients/times.js
+++ SRC/clients/times.js
@@ -3,16 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 
-//sem todos os times
-// function carregarListaTimes() {
-//     fetch(`http://localhost:3000/api/times/${paisEscolhido}`)
-//         .then(response => response.json())
-//         .then(data => mostrarListaTimes(data))
-//         .catch(error => console.error("Erro:", error))
-//         console.log('ola' + paisEscolhido)
-// }
-
-
 //com todos os times
 function carregarListaTimes() {
     fetch(`http://localhost:3000/api/times/${paisEscolhido}`)
@@ -65,7 +55,7 @@ function mostrarListaTimes(data) {
 }
 
 
-
+/*
 function carregarDetalhesTime(id, liga) {
 
     //depois melhorar
@@ -87,8 +77,8 @@ function carregarDetalhesTime(id, liga) {
         })
         .catch(error => console.error("Erro:", error))
 }
-
-
+*/
+/*
 function criarDialog(time) {
     let dialog = document.createElement('dialog')
     let cabecalho = document.createElement('div')
@@ -156,6 +146,7 @@ function criarDialog(time) {
 
     dialog.showModal()
 }
+*/
 
 
 
@@ -214,37 +205,375 @@ function criarDialog(time) {
 
 
 
-//////////////////////////// testando pegar elementos
 
 
-function mostrarDetalhesTime(id, pais) { //depois trocar, agora coloquei pais nos times, nao precisa mais pegar pela liga
 
-    //depois melhorar
-    // if(liga != undefined) {
-    //     if(liga === 'Bundesliga') {
-    //         paisEscolhido = 'Alemanha'
-    //     } else if(liga === 'Premier League') {
-    //         paisEscolhido = 'Inglaterra'
-    //     } else if(liga === 'Brasileirão') {
-    //         paisEscolhido = 'Brasil'
-    //     }  else if(liga === 'La Liga') {
-    //         paisEscolhido = 'Espanha'
-    //     }
+
+
+
+//backup pra quando mostrar todos, contem a bandeira e o logo da liga
+function mostrarListaTodosTimes(data) {
+    
+    data.sort((a,b) => (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0))
+
+    let listaDeTimes = document.createElement('section')
+    listaDeTimes.id = 'listaDeTimes'
+    listaDeTimes.className = 'grid grid-cols-5 gap-x-6 gap-y-10 p-8'
+
+    let main = document.getElementById('main')
+
+    main.appendChild(listaDeTimes)
+    
+
+    data.forEach(time => {
+        let cardTime = document.createElement('div')
+        cardTime.className = 'group p-4 border border-black bg-white hover:cursor-pointer hover:shadow-xl hover:bg-[#ebe8e8] transition duration-300 ease-in-out'
         
+        let cardTimeConteudo = document.createElement('div')
+        cardTimeConteudo.className = "grid grid-rows-[80px 175px 105px] gap-y-7 justify-items-center text-center h-full bg-[#ebe8e8] pt-5 pb-8 border border-black group-hover:opacity-80 group-hover:border-[#ebe8e8] transition ease-in-out duration-300"
         
-    //}
+        cardTime.appendChild(cardTimeConteudo)
+
+        let logoLiga = document.createElement('img')
+        logoLiga.src=`../img/ligas/${time.liga}.png`
+        logoLiga.className = 'w-2/6'
+      
+        cardTimeConteudo.appendChild(logoLiga)
+
+        let escudo = document.createElement('img')
+        escudo.src = time.escudo
+        cardTimeConteudo.appendChild(escudo)
+
+        let nome = document.createElement('h2')
+        nome.innerText = time.nome
+        cardTimeConteudo.appendChild(nome)
+        
+        cardTime.addEventListener('click', function() {
+            carregarDetalhesTime(time.id, time.pais)
+        })
+
+        listaDeTimes.appendChild(cardTime)
+       
+    })
+}
+
+
+function carregarDetalhesTime(id, pais) {
+
     fetch(`http://localhost:3000/api/times/${pais}/${id}`)
         .then(response => response.json())
         .then(function(data) {
-            criarNovoDialog(data)
+            criarDialog(data)
         })
         .catch(error => console.error("Erro:", error))
 }
 
 
-function criarNovoDialog(time) {
+
+function criarDialog(time) {
+    
+    let dialog = document.createElement('dialog')
+    
+    let cabecalho = document.createElement('div')
+    cabecalho.className = `h-64 bg-[${time.cores[0]}] flex mb-6 shadow-md pr-10`
+
+    let divEscudo = document.createElement('div')
+    divEscudo.className = "flex justify-center items-center px-5 m-4 w-1/4"
+
+    let escudo = document.createElement('img')
+    // escudo.style.height = '90%'
+    escudo.className = "h-5/6"
+    escudo.src = time.escudo.replace('small', 'big')
+    divEscudo.appendChild(escudo)
+
+    cabecalho.appendChild(divEscudo)
+
+    let divNomeApelido = document.createElement('div')
+    divNomeApelido.className = "flex w-full flex-col justify-center items-center gap-8 pr-5"
+
+    let nomeTime = document.createElement('h1')
+    nomeTime.className = `text-5xl font-bold text-[${time.cores[1]}]`
+    nomeTime.innerText = time.nome_completo
+
+    divNomeApelido.appendChild(nomeTime)
+
+    let apelido = document.createElement('h2')
+    apelido.className = `text-2xl font-bold text-[${time.cores[1]}]`
+    apelido.innerText = `"${time.apelido}"`
+
+    divNomeApelido.appendChild(apelido)
+
+    cabecalho.appendChild(divNomeApelido)
+
+    dialog.appendChild(cabecalho)
+
+    //depois ver se faço abas, uma div com scroll(cabeçalho fixo) ou tudo com scroll
+
+    let divResumo = document.createElement('div')
+    // divResumo.className = 'm-4 px-8 pt-8 pb-16 border-2 border-gray-300'
+    divResumo.className = 'my-4 px-12 pt-8 pb-14 border-b-2 border-t-2 border-gray-200 bg-gray-100'
+    // divResumo.className = 'm-4 px-8 pt-8 pb-16 border-2 border-gray-300'
+
+    let tituloResumo = document.createElement('h3')
+    tituloResumo.className = 'text-xl border-b-2 border-gray-300 mb-6 font-medium'
+    tituloResumo.innerText = 'Resumo:'
+
+    divResumo.appendChild(tituloResumo)
+
+    let resumo = document.createElement('p')
+    resumo.className = 'text-lg'
+    resumo.innerText = time.resumo
+
+    divResumo.appendChild(resumo)
+
+    dialog.appendChild(divResumo)
+
+
+    let divUniformes = document.createElement('div')
+    divUniformes.className = 'my-4 px-12 pt-8 pb-14 border-b-2 border-t-2 border-gray-200 bg-gray-100'
+
+    let tituloUniformes = document.createElement('h3')
+    tituloUniformes.className = 'text-xl border-b-2 border-gray-300 mb-6 font-medium'
+    tituloUniformes.innerText = 'Uniformes:'
+
+    let divImagensUniformes = document.createElement('div')
+    divImagensUniformes.className = 'flex gap-12 justify-center'
+
+    time.uniformes.forEach(uniforme => {
+        let div = document.createElement('div')
+        div.className = 'flex flex-col justify-center items-center gap-4'
+        let modelo = document.createElement('h3')
+        modelo.innerText = uniforme.modelo
+        let img = document.createElement('img')
+        img.src = uniforme.img
+
+        div.appendChild(modelo)
+        div.appendChild(img)
+
+        divImagensUniformes.appendChild(div)
+        
+    });
+
+    divUniformes.appendChild(tituloUniformes)
+
+    divUniformes.appendChild(divImagensUniformes)
+
+    dialog.appendChild(divUniformes)
+
+
+    let divEstadio = document.createElement('div')
+    divEstadio.className = 'my-4 px-12 pt-8 pb-14 border-b-2 border-t-2 border-gray-200 bg-gray-100'
+
+    let tituloEstadio = document.createElement('h3')
+    tituloEstadio.className = 'text-xl border-b-2 border-gray-300 mb-6 font-medium'
+    tituloEstadio.innerText = 'Estádio:'
+
+    let divInfoEstadio = document.createElement('div')
+    divInfoEstadio.className = 'flex flex-col gap-4 justify-center items-center'
+
+    let nomeEstadio = document.createElement('h3')
+    nomeEstadio.className = 'font-semibold'
+    nomeEstadio.innerText = time['estadio']['nome']
+
+    let fotoEstadio = document.createElement('img')
+    fotoEstadio.className = 'w-1/2'
+    fotoEstadio.src = time.estadio.foto
+
+    divInfoEstadio.appendChild(nomeEstadio)
+    divInfoEstadio.appendChild(fotoEstadio)
+
+    divEstadio.appendChild(tituloEstadio)
+
+    let divInauguracaoCapacidadeEstadio = document.createElement('div')
+    divInauguracaoCapacidadeEstadio.className = 'flex justify-between w-2/4 px-8'
+
+    let inauguracao = document.createElement('h3')
+    inauguracao.innerHTML = `<b>Inauguração:</b> ${time['estadio']['inauguracao']}`
+
+    let capacidade = document.createElement('h3')
+    capacidade.innerHTML = `<b>Capacidade:</b> ${time['estadio']['capacidade']}`
+
+    divInauguracaoCapacidadeEstadio.appendChild(inauguracao)
+    divInauguracaoCapacidadeEstadio.appendChild(capacidade)
+
+    divInfoEstadio.appendChild(divInauguracaoCapacidadeEstadio)
+
+    divEstadio.appendChild(divInfoEstadio)
+
+    dialog.appendChild(divEstadio)
+
+
+
+
+    let corpo = document.createElement('div')
+    corpo.style.display = 'flex'
+    // corpo.style.height = '320px'
+    corpo.style.backgroundColor = '#f5a000'
+    corpo.style.color = '#ffffff'
+    // corpo.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eos, harum modi alias accusamus odio, voluptatibus iusto eius sequi porro excepturi reiciendis animi, voluptates dicta! Non sapiente voluptatum numquam ex? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eos, harum modi alias accusamus odio, voluptatibus iusto eius sequi porro excepturi reiciendis animi, voluptates dicta! Non sapiente voluptatum numquam ex? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eos, harum modi alias accusamus odio, voluptatibus iusto eius sequi porro excepturi reiciendis animi, voluptates dicta! Non sapiente voluptatum numquam ex?"
+    // cabecalho.className = 'cabeçalho'
+    // cabecalho.style.backgroundColor = time.cores[0]
+    // cabecalho.style.color = time.cores[1]
+    // let logoCard = document.createElement('div')
+    // logoCard.style.backgroundColor = '#ffffff'
+    // let logo = document.createElement('img')
+    // logo.src = time.escudo.replace('small', 'big')
+    // logoCard.appendChild(logo)
+    // header.appendChild(logoCard)
+    
+    dialog.appendChild(corpo)
+
+
+    console.log('aqui')
+    console.log(time['uniforme'])
+
+    let uniformes = time['uniformes']
+
+    console.log(uniformes)
+
+    uniformes.forEach(uniforme => {
+        let img = document.createElement('img')
+        img.src = uniforme.img
+        let modelo = document.createElement('h5')
+        modelo.innerText = uniforme.modelo
+        corpo.appendChild(modelo)
+        corpo.appendChild(img)
+    });
+
+    let estadioNome = document.createElement('h1')
+    estadioNome.innerHTML = `Estádio: ${time['estadio'].nome}`
+
+    corpo.appendChild(estadioNome)
+
+
+    // let fotoEstadio = document.createElement('img')
+    // fotoEstadio.src = time['estadio'].foto
+
+    // corpo.appendChild(fotoEstadio)
+
+
+    console.log(time.titulos)
+
+
+    document.body.appendChild(dialog)
+
+    console.log(time.titulos)
+
+    let titulos = time.titulos
+
+    titulos.forEach(titulo => {
+        console.log(titulo.competicao)
+        console.log(titulo.edicoes.toString().replaceAll(',', ', '))
+
+        let nomeTitulo = document.createElement('h2')
+        nomeTitulo.innerHTML = `${titulo.competicao} (${titulo.edicoes.length})`
+        let tituloEdicoes = document.createElement('p')
+        tituloEdicoes.innerHTML = titulo.edicoes.toString().replaceAll(',', ', ')
+        dialog.appendChild(nomeTitulo)
+        dialog.appendChild(tituloEdicoes)
+    });
+
+    console.log('dados pais')
+    console.log(time.pais)
+
+    console.log(`http://localhost:3000/api/paises/${time.pais}`)
+
+    fetch(`http://localhost:3000/api/paises/${time.pais}`)
+    .then(response => response.json())
+    .then(function(data) {
+        console.log('pais11:')
+        console.log(data.campeonatos)
+        console.log(data.copas)
+
+        data.campeonatos.forEach(campeonato => {
+            let img1 = document.createElement('img')
+            let img2 = document.createElement('img')
+            img1.src = campeonato.logo
+            img2.src = campeonato.trofeu
+            dialog.appendChild(img1)
+            dialog.appendChild(img2)
+        });
+
+        data.copas.forEach(copa => {
+            let img1 = document.createElement('img')
+            let img2 = document.createElement('img')
+            img1.src = copa.logo
+            img2.src = copa.trofeu
+            dialog.appendChild(img1)
+            dialog.appendChild(img2)
+        });
+
+        let img = document.createElement('img')
+        img.src = data.bandeira
+        dialog.appendChild(img)
+    })
+    .catch(error => console.error("Erro:", error))
+
+
+
+    let info = document.createElement('div')
+    let infoConteudo = document.createElement('p')
     
     
+    infoConteudo.innerHTML = '<b>Nome Completo:</b> ' + time.nome_completo + '<br>'
+
+    infoConteudo.innerHTML += '<b>Fundação:</b> ' + time.fundacao + '<br>'
+
+    infoConteudo.innerHTML += '<b>Cidade:</b> ' + time.cidade + '<br>'
+
+    infoConteudo.innerHTML += '<b>Estado:</b> ' + time.estado + '<br>'
+
+    infoConteudo.innerHTML += '<b>País:</b> ' + time.pais + '<br>'
+
+    infoConteudo.innerHTML += '<b>Liga:</b> ' + time.liga + '<br>'
+
+    infoConteudo.innerHTML += '<b>Presidente:</b> ' + time.presidente + '<br>'
+
+    infoConteudo.innerHTML += '<b>Website:</b> ' + time.website + '<br>'
+
+    infoConteudo.innerHTML += '<b>E-mail:</b> ' + time.email + '<br>'
+
+    infoConteudo.innerHTML += '<b>Marca Uniforme:</b> ' + time.marca_uniforme + '<br>'
+
+
+    infoConteudo.innerHTML += '<br><br><br>' + time.resumo
+
+    info.appendChild(infoConteudo)
+    dialog.appendChild(info)
+
+
+    fetch(`http://localhost:3000/api/paises/${time.pais}/${time.liga}`)
+    .then(response => response.json())
+    .then(function(data) {
+        console.log('liga')
+        console.log(data)
+
+        let logoLiga = document.createElement('img')
+        logoLiga.src = data.logo
+        dialog.appendChild(logoLiga)
+        
+    })
+    .catch(error => console.error("Erro:", error))
+
+    
+
+    dialog.showModal()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//backup
+/* 
+function criarDialog(time) {
     
     let dialog = document.createElement('dialog')
     let cabecalho = document.createElement('div')
@@ -445,64 +774,4 @@ function criarNovoDialog(time) {
 
     dialog.showModal()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//backup pra quando mostrar todos, contem a bandeira e o logo da liga
-function mostrarListaTodosTimes(data) {
-    
-    data.sort((a,b) => (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0))
-
-    let listaDeTimes = document.createElement('section')
-    listaDeTimes.id = 'listaDeTimes'
-    listaDeTimes.className = 'grid grid-cols-5 gap-x-6 gap-y-10 p-8'
-
-    let main = document.getElementById('main')
-
-    main.appendChild(listaDeTimes)
-    
-
-    data.forEach(time => {
-        let cardTime = document.createElement('div')
-        cardTime.className = 'group p-4 border border-black bg-white hover:cursor-pointer hover:shadow-xl hover:bg-[#ebe8e8] transition duration-300 ease-in-out'
-        
-        let cardTimeConteudo = document.createElement('div')
-        cardTimeConteudo.className = "grid grid-rows-[80px 175px 105px] gap-y-7 justify-items-center text-center h-full bg-[#ebe8e8] pt-5 pb-8 border border-black group-hover:opacity-80 group-hover:border-[#ebe8e8] transition ease-in-out duration-300"
-        
-        cardTime.appendChild(cardTimeConteudo)
-
-        let logoLiga = document.createElement('img')
-        logoLiga.src=`../img/ligas/${time.liga}.png`
-        logoLiga.className = 'w-2/6'
-      
-        cardTimeConteudo.appendChild(logoLiga)
-
-        let escudo = document.createElement('img')
-        escudo.src = time.escudo
-        cardTimeConteudo.appendChild(escudo)
-
-        let nome = document.createElement('h2')
-        nome.innerText = time.nome
-        cardTimeConteudo.appendChild(nome)
-        
-        cardTime.addEventListener('click', function() {
-            mostrarDetalhesTime(time.id, time.pais)
-        })
-
-        listaDeTimes.appendChild(cardTime)
-       
-    })
-}
+*/
