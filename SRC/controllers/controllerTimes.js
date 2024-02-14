3c2f01603f3e09b865f692a7e887e1e81d005a3f
rw09 <rodrigosouza170487@gmail.com>
2024-02-13 22:52:26 -0300

    Testes na Rota para salvar as imagens usando o express-fileupload

--- SRC/controllers/controllerTimes.js
+++ SRC/controllers/controllerTimes.js
@@ -3,23 +3,26 @@ const server = express()
 // const dados = require('./data/dadosTimes.json')
 const dados = require('./data/dadosTimes2.json')
 const fs = require('fs')
+const path = require('path'); 
 
+const fileUpload = require('express-fileupload');
+server.use(fileUpload());
 
 server.use(express.json())
 
+
 server.get('/times/Todos', (req, res) => {
-    const todosOsTimes = [];
-    for (const pais in dados) {
+    
+    const times = [];
+    
+    for(const pais in dados) {
         for(const time of dados[pais]) {
-            todosOsTimes.push(time)
+            times.push(time)
         }
     }
-    console.log('todos os times')
-    console.log(todosOsTimes)
-    
-    return res.json(todosOsTimes);
-    
-  }); 
+        
+    return res.json(times);
+}) 
 
 server.get('/times/:pais', (req, res) => {
 
@@ -49,30 +52,58 @@ server.get('/times/:pais/:id', (req, res) => {
 })
 
 
-/*
-server.get('/times/Alemanha', (req, res) => {
-    return res.json(dados.TimesAlemanha)
+server.post('/times', function(req, res)  {
+    
+    const novoTime = req.body;
+
+    let fotoEstadio;
+    let uploadPath;
+
+    let paisDoTime = 'Tokyio'
+
+    if (!fs.existsSync(`src/img/estadios/${paisDoTime}`)) {
+        fs.mkdirSync(`src/img/estadios/${paisDoTime}`, { recursive: true });
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    fotoEstadio = req.files.fotoEstadio;
+    // uploadPath = path.join(__dirname, '..', 'hello', fotoEstadio.name)
+    // uploadPath = path.join(__dirname, '..', 'img', 'estadios', paisDoTime, fotoEstadio.name)
+    uploadPath = `src/img/estadios/${paisDoTime}/${fotoEstadio.name}`
+
+    console.log(uploadPath)
+
+    // Use the mv() method to place the file somewhere on your server
+    fotoEstadio.mv(uploadPath, function(err) {
+        if (err)
+        return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+    
+    // res.status(200).json({ mensagem: "Time criado com sucesso" });
 })
 
-server.get('/times/Brasil', (req, res) => {
-    return res.json(dados.TimesBrasil)
+
+server.delete("/times/:pais/:id", (req, res) => {
+    const timeId = parseInt(req.params.id)
+
+    console.log('time idddd: ' + timeId)
+    const paisTime = req.params.pais
+
+    dados[`Times${paisTime}`] = dados[`Times${paisTime}`].filter(t => t.id !== timeId)
+    // dados.TimesAlemanha = dados.TimesAlemanha.filter(u => u.id !== timeId)
+
+    console.log(dados[`Times${paisTime}`])
+
+    salvarDados(dados)
+
+    return res.status(200).json({mensagem: "Time excluido com sucesso"})
 })
 
-server.get('/times/Espanha', (req, res) => {
-    return res.json(dados.TimesEspanha)
-})
+function salvarDados(){
+    // fs.writeFileSync(__dirname + '/data/dadosTimes2.json', JSON.stringify(dados, null, 2))
+    fs.writeFileSync(__dirname + '/data/dadosTimes.json', JSON.stringify(dados, null, 2))
+}
 
-server.get('/times/Franca', (req, res) => {
-    return res.json(dados.TimesFranca)
-})
-
-server.get('/times/Inglaterra', (req, res) => {
-    return res.json(dados.TimesInglaterra)
-})
-
-server.get('/times/Italia', (req, res) => {
-    return res.json(dados.TimesItalia)
-})
-*/
-
-module.exports = {server}
+module.exports = {server, salvarDados}
