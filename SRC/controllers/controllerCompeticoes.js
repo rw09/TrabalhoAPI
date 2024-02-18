719c4d089e6e37e2cdf716b6e72f95762f35a554
rw09 <rodrigosouza170487@gmail.com>
2024-02-18 12:10:35 -0300

    Alterações para buscar as informações para o cadastro de time

--- SRC/controllers/controllerCompeticoes.js
+++ SRC/controllers/controllerCompeticoes.js
@@ -26,4 +26,23 @@ server.get('/competicoes/:nome', (req, res) => {
     return res.json(competicao);
 })
 
+//pegar competições pelo nivel
+server.get('/competicoes/continental/:continente', (req, res) => {
+    
+    let continente = req.params.continente
+    console.log('params ' + req.params.continente)
+
+    const competicaoNivel = `Continental - ${continente}`
+    console.log('comp + ' + competicaoNivel)
+
+    const competicao = dados.Competicao.filter(c => c.nivel === competicaoNivel)
+
+    if (!competicao) {
+        return res.status(404).json({mensagem: "Competição não encontrada :/"})
+    }
+    
+    return res.json(competicao);
+    // return res.json({ola})
+})
+
 module.exports = {server}
